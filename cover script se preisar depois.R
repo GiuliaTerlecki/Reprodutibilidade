1ed558f9204b072857d1a3669bca4135acb75371
Giulia Terlecki <giuliaterlecki@gmail.com>
2024-11-04 23:09:24 -0300

    f

---
+++ cover script se preisar depois.R
@@ -0,0 +1,22 @@
+git status
+<style>
+  body {
+    background-image: url('https://raw.githubusercontent.com/GiuliaTerlecki/Reprodutibilidade/main/fundotuba.png');
+    background-size: cover; 
+    background-position: center;
+    background-attachment: fixed;
+  }
+
+h1, h2, p {
+  color: white;
+}
+</style>
+  
+  <!-- Conteúdo do site -->
+  
+  </body>
+  
+  </html>
+  
+  <head><link rel="icon" href="https://github.com/GiuliaTerlecki/Reprodutibilidade/blob/main/icone.ico" type="image/x-icon"></head>
+  
